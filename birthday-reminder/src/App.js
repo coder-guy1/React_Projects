d3a7297530e4c1b100493ba6a776edb4e6beefee
coder-guy1 <abhijeetingh11705@gmail.com>
2023-11-09 21:48:56 +0530

    final commit

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -3,7 +3,18 @@ import data from "./data";
 import List from "./list";
 
 function App() {
-  return <h1>helloo</h1>;
+  const [people, setPeople] = useState(data);
+  return (
+    <>
+      <main>
+        <section className="container">
+          <h3>{people.length} birthdays today</h3>
+          <List people={people} />
+          <button onClick={() => setPeople([])}>clear All</button>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default App;
