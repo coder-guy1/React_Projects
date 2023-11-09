7212f0525efa99917c720ce3e126732e3069038e
coder-guy1 <abhijeetingh11705@gmail.com>
2023-11-09 20:53:19 +0530

    initial commit

---
+++ birthday-reminder/src/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./list";
+
+function App() {
+  return <h1>helloo</h1>;
+}
+
+export default App;
---
+++ birthday-reminder/src/list.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const list = () => {
+  return (
+    <>
+      <h2>list component</h2>
+    </>
+  );
+};
+export default list;
